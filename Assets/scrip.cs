710f944ffa373900e399241b784b608c23575420
passsist11 <42786082+passsist11@users.noreply.github.com>
2018-09-25 00:10:19 -0400

    Add files via upload

---
+++ Assets/scrip.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrip : MonoBehaviour
+{
+
+    public Material[] material;
+    Renderer rend;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Box")
+        {
+            rend.sharedMaterial = material[1]; 
+
+        }
+   
+
+    }
+
+}
+
